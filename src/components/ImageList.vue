b302663766ad860d19e5dadac9f22d3352040c44
zeze.li <lizeze.boy@gmail.com>
2020-11-01 23:27:50 +0800

    first commit

---
+++ src/components/ImageList.vue
@@ -0,0 +1,168 @@
+<template>
+
+    <div class="image-list-container">
+
+        <div class="image-list">
+            <div class="item">
+                <div class="image">
+                    <img src="../assets/logo.png" alt="">
+                </div>
+                <div class="more-info">
+                    <div class="name">
+
+                        <span>中式实木衣柜组装简约现代经济小户型木质衣橱卧室收纳6门大衣柜 胡桃色 6门 组装</span>
+                    </div>
+
+                    <div class="price">
+
+                        <span>￥23</span>
+
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="image">
+
+                    <img src="../assets/logo.png" alt="">
+                </div>
+                <div class="more-info">
+                    <div class="name">
+
+                        <span>中式实木衣柜组装简约现代经济小户型木质衣橱卧室收纳6门大衣柜 胡桃色 6门 组装</span>
+                    </div>
+                    <div class="price">
+
+                        <span>￥23</span>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="item">
+                <div class="image">
+
+                    <img src="../assets/logo.png" alt="">
+                </div>
+                <div class="more-info">
+                    <div class="name">
+
+                        <span>中式实木衣柜组装简约现代经济小户型木质衣橱卧室收纳6门大衣柜 胡桃色 6门 组装</span>
+                    </div>
+                    <div class="price">
+
+                        <span>￥23</span>
+
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="image">
+
+                    <img src="../assets/logo.png" alt="">
+                </div>
+                <div class="more-info">
+                    <div class="name">
+
+                        <span>中式实木衣柜组装简约现代经济小户型木质衣橱卧室收纳6门大衣柜 胡桃色 6门 组装</span>
+                    </div>
+                    <div class="price">
+
+                        <span>￥23</span>
+
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="image">
+
+                    <img src="../assets/logo.png" alt="">
+                </div>
+                <div class="more-info">
+                    <div class="name">
+
+                        <span>中式实木衣柜组装简约现代经济小户型木质衣橱卧室收纳6门大衣柜 胡桃色 6门 组装</span>
+                    </div>
+                    <div class="price">
+
+                        <span>￥23</span>
+
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="image">
+
+                    <img src="../assets/logo.png" alt="">
+                </div>
+                <div class="more-info">
+                    <div class="name">
+
+                        <span>中式实木衣柜组装简约现代经济小户型木质衣橱卧室收纳6门大衣柜 胡桃色 6门 组装</span>
+                    </div>
+                    <div class="price">
+
+                        <span>￥23</span>
+
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="image">
+
+                    <img src="../assets/logo.png" alt="">
+                </div>
+                <div class="more-info">
+                    <div class="name">
+
+                        <span>中式实木衣柜组装简约现代经济小户型木质衣橱卧室收纳6门大衣柜 胡桃色 6门 组装</span>
+                    </div>
+                    <div class="price">
+
+                        <span>￥23</span>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ImageList'
+    }
+</script>
+
+<style scoped lang="scss">
+
+    .image-list-container {
+        margin: 30px;
+
+        .image-list {
+            display: flex;
+            align-content: start;
+            justify-content: start;
+            flex-wrap: wrap;
+
+            .item {
+                margin: 5px;
+                width: 230px;
+
+                .more-info {
+                    .name {
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                        display: -webkit-box;
+                        -webkit-box-orient: vertical;
+                        -webkit-line-clamp: 2;
+
+
+                    }
+                }
+
+            }
+        }
+    }
+
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+     <image-list></image-list>
+
+</div>
+</template>
+
+<script>
+    import ImageList from "../components/ImageList";
+    export default {
+        name: 'Index',
+        components: {ImageList}
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
